30f91f4ac723b485e6d145071d16b5e951b1dc39
Jillian Ma <ma.jillian@gmail.com>
2021-12-16 11:57:23 -0500

    Create factorial.c

---
+++ recursion/factorial.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int factorial(int n);
+
+int main(void)
+{
+    int number = get_int("Number: ");
+    int answer = factorial(number);
+    printf("%i! = %i", number, answer);
+    
+}
+
+int factorial(int n)
+{
+    if (n == 0)
+        return 1;
+    return n * factorial(n - 1);
+}
