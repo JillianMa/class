70da6bf288c39c03053ef552906df65138cb1982
Jillian Ma <ma.jillian@gmail.com>
2023-03-01 09:31:54 -1000

    Update caesar.py

--- create/caesar.py
+++ create/caesar.py
@@ -1,4 +1,4 @@
-ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ALPHABET = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
 def encrypt(word, key):
     encrypted_result = ""
